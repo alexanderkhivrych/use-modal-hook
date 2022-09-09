dc685cfe752319dfb4fd94cfb1f1d9a1691c9292
alexanderkhivrych <alexander.khivrych@gmail.com>
2022-09-09 10:47:39 +0200

    feat(app): update reactjs

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
-    library: '',
-    libraryTarget: 'commonjs'
   },
+  target: 'web',
   externals: [nodeExternals()],
   module: {
     rules: [
