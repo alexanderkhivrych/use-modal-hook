0ef5fbde383a06a05066a1e60d09ef55ceb28ded
alexanderkhivrych <alexander.khivrych@gmail.com>
2022-09-09 11:39:11 +0200

    fix: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
+    libraryTarget: 'commonjs'
   },
-  target: 'web',
   externals: [nodeExternals()],
   module: {
     rules: [
